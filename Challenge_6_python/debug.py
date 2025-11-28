76ac5b32f5e5e2dbb648b3171f5aee8a39dbb362
Christian R <cjromerom28@gmail.com>
2025-11-27 21:01:20 -0500

    Advances

---
+++ Challenge_6_python/debug.py
@@ -0,0 +1,10 @@
+numero1=10
+numero2=20
+suma=numero1+numero2
+resta=numero1-numero2
+multiplicacion=numero1*numero2
+division=numero1/numero2
+print(f"Respuesta de la suma es: {suma}")
+print(f"Respuesta de la resta es: {resta}")
+print(f"Respuesta de la multiplicacion es: {multiplicacion}")
+print(f"Respuesta de la division es: {division}")
