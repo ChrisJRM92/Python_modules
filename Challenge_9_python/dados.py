032e85ddb41d346ead62f29d8b95ecfb898df1c0
Christian R <cjromerom28@gmail.com>
2025-11-29 12:31:05 -0500

    Advances

---
+++ Challenge_9_python/dados.py
@@ -0,0 +1,10 @@
+import random
+print("******Prueba tu suerte*******")
+print("Reglas: Si el valor lanzado es mayor a 3 ganas el juego")
+input("Presiona enter para lanzar el dado...")
+valorDado = random.randint(1,6)
+print(f"Tu valor es: {valorDado}")
+
+if valorDado > 3:
+    print("Ganaste felcitaciones!!")
+    print("Estas de suerte")
