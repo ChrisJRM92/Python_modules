93b13b364158dbdc2ee98488acd2e742ed3e72a6
Christian R <cjromerom28@gmail.com>
2026-01-04 18:54:37 -0500

    Advances

---
+++ Challenge_28_JuegoDados/utilitariosImagen.py
@@ -0,0 +1,16 @@
+from PIL import Image, ImageTk
+
+#Cambiar PATH
+PATH="C:\\Users\\DEV-CR\\Desktop\\ProyectosGIT\\Python_modules\\Challenge_28_JuegoDados\\"
+
+def obtenerImagen(nombre,ancho=300):
+    imagen = Image.open(PATH+nombre)
+    porcentaje = (ancho / float(imagen.size[0]))
+    alto = int((float(imagen.size[1]) * float(porcentaje)))
+    imagenModificada=imagen.resize((ancho, alto), Image.Resampling.LANCZOS)
+    photoImg=ImageTk.PhotoImage(imagenModificada)
+
+    return photoImg
+
+
+
